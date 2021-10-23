85fa27022fcb5de446c07831fa500dd8b2bcf139
Tareque Khan <tarequk@amazon.com>
2021-10-23 19:12:02 +0530

    serverless aws-node-express-api-app RESTAPI use-case

---
+++ handler.js
@@ -0,0 +1,28 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+app.get("/mtk", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from mtk!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
